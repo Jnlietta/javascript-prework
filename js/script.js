2ab7ad36718575442d5769b9838a980f2767afad
Julia Glinka <julia.w.glinka@gmail.com>
2023-09-05 12:56:00 +0200

    Add basic function getMoveName

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
+function getMoveName(argMoveId){
+  if(argMoveId == 1){
+    return 'kamień';
+  }
+
+  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+  return 'nieznany ruch';
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
+/*if(randomNumber == 1){
   computerMove = 'kamień';
 } else if (randomNumber == 2){
   computerMove = 'papier';
 } else if (randomNumber == 3){
   computerMove = 'nożyce';
-}
+}*/
 
 printMessage('Mój ruch to: ' + computerMove);
 
